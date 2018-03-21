45612cb6d05d7bbaf19de1ef1264d676db633e22
fowlerae <fowlerae@rose-hulman.edu>
2018-03-20 22:12:09 -0400

    made changes

--- src/problem4.py
+++ src/problem4.py
@@ -2,8 +2,8 @@
 Exam 1, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Ashley Fowler.  March 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -135,10 +135,14 @@ def problem4(m):
                                  (i.e., it is prime)
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    sum = 0
+    for k in range(m-3):
+        if m % (k+2) == 0:
+            sum = sum + 1
+    return sum
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
